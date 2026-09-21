104c8ca50fa66035af5aa38ef8888d29b2334ca4
Varun Kumar Polumuri <varunkohli1817@gmail.com>
2026-09-21 19:31:02 +0530

    Time: 2155 ms (5.07%), Space: 14.3 MB (21.94%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    vector<int>ans;
-    vector<bool>marked;
-    void solve(vector<int>&nums,string temp,vector<bool>&vis){
-        if(temp.size()==3){
-            if(temp[0]!='0'&&(temp[2]-'0')%2==0) 
-            {
-                if(!marked[stoi(temp)]) ans.push_back(stoi(temp));
+    vector<int> ans;
+    vector<bool> marked;
+    void solve(vector<int>& nums, string temp, vector<bool>& vis) {
+        if (temp.size() == 3) {
+            if (temp[0] != '0' && (temp[2] - '0') % 2 == 0) {
+                if (!marked[stoi(temp)])
+                    ans.push_back(stoi(temp));
                 marked[stoi(temp)] = true;
             }
-            return; 
+            return;
         }
 
-        for(int i=0;i<nums.size();i++){
-            if(!vis[i]){
-                vis[i] = true;
-                temp.push_back(nums[i]+'0');
-                solve(nums,temp,vis);
-                vis[i] = false;
-                temp.pop_back();
+        for (int i = 0; i < nums.size(); i++) {
+            if (temp.size() <= 3) {
+                if (!vis[i]) {
+                    vis[i] = true;
+                    temp.push_back(nums[i] + '0');
+                    solve(nums, temp, vis);
+                    vis[i] = false;
+                    temp.pop_back();
+                }
             }
         }
-
     }
     vector<int> findEvenNumbers(vector<int>& nums) {
         int n = nums.size();
-        marked.assign(1000,false);
-        vector<bool>vis(n,false);
-        solve(nums,"",vis);
-        sort(ans.begin(),ans.end());
+        marked.assign(1000, false);
+        vector<bool> vis(n, false);
+        solve(nums, "", vis);
+        sort(ans.begin(), ans.end());
         return ans;
     }
 };
